6381ae51560a7dd4f7da22f025f66a360a58170d
Christopher Corvo <christophercorvo@gmail.com>
2020-10-27 21:31:22 -0400

    added styles

--- user-card/src/components/UserCard.js
+++ user-card/src/components/UserCard.js
@@ -1,33 +1,93 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const UserCard = (props) => {
-    const { data, followerData } = props
+const UserPage = styled.div`
+    display: flex;
+    flex-direction: column;
 
-    console.log(followerData[0].login)
-    return (
-        <div className="UserCard">
-            <div className="User_Container">
-                <img src={data.avatar_url} alt={data.avatar_url}/>
-                <h3> Name: {data.name}</h3>
-                <h3> Username:{data.login}</h3>
-            </div>
+    
+    .User_Container{ 
+        display: flex;
+        flex-direction: column;
+        
+        
+    }
 
-            <div className='followers'>
-                <h2> Follower: </h2>
-            {followerData.map(follower => {
-                return (
-                    <div>
-                        <img src={follower.avatar_url} alt={follower.avatar_url}/>
-                        <h3>Name: {follower.name}</h3>
-                        <h3>Username: {follower.login}</h3>
+    .mainUser{
+        display: flex;
+        flex-direction:column;
+        border: 1px solid black;
+        width: 20%;
+        align-self: center;
+        margin-top: 3%;
+        margin-bottom: 2%;
+    }
+
+    .profileInfo{
+        background: mistyrose;
+    }
+`
+
+const Followers = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    
+    
+
+    .follower{
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        margin-left: 1%;
+        margin-top: 1%;
+        width: 20%;
+        
+    }
+
+    img{
+        max-width: 100%;
+        align-self: center;
+        
+    }
+`
+
+class UserCard extends React.Component {
+ 
+    render() {
+        
+        return (
+            <UserPage>
+                <div className="User_Container">
+                    <div className='mainUser'>
+                        <img src={this.props.data.avatar_url} alt={this.props.data.avatar_url}/>
+                        <div className='profileInfo'>
+                            <h3> Name: {this.props.data.name}</h3>
+                            <h3> Username:{this.props.data.login}</h3>
+                        </div>
                     </div>
-                )
-            })}
-            </div>
-        </div>
-    )
+                </div>
+                <h2> Follower: </h2>
+                <Followers>
+                    
+                    {this.props.followerData.map(follower => {
+                    return (
+                        <div className='follower'>
+                            <img src={follower.avatar_url} alt={follower.avatar_url}/>
+                            
+                            <div className='profileInfo'> 
+                                <h3>Name: {follower.name}</h3>
+                                <h3>Username: {follower.login}</h3>
+                            </div>
+                        </div>
+                    )
+                })}
+                </Followers>
+            </UserPage>
+        )
+    }
 }
-
 export default UserCard
 
 
